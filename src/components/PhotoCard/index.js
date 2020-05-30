98824219abf075409039f7a6fb881a6c83c4f874
Samuel Mata <Samuelab2@gmail.com>
2020-05-29 21:38:50 -0400

    mutation to count likes and using render props

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,8 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Article, ImgWrapper, Img, Button } from './styles'
-import { FcLike, FcLikePlaceholder } from "react-icons/fc";
+import React from 'react'
+import { Article, ImgWrapper, Img } from './styles'
+
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
+
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToggleLikeMutation'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -10,8 +13,6 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, ref] = useNearScreen()
   const key = `like-${id}`
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  const Icon = liked ? FcLike : FcLikePlaceholder
 
   return (
     <Article ref={ref}>
@@ -23,9 +24,19 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             <Img src={src} />
           </ImgWrapper>
           </a>
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size='32px' /> {likes} likes!
-          </Button>
+          <ToggleLikeMutation>
+            {
+              (toggleLike) => {
+                const handleFavClick = () => {
+                  !liked && toggleLike({ variables: {
+                    input: { id }
+                  } })
+                  setLiked(!liked)
+                }
+                return <FavButton liked={liked} likes={likes} onClick={handleFavClick}/>
+              }
+            }
+          </ToggleLikeMutation>
         </>
       }
     </Article>
