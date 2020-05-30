ae586e3e635e33b5d84a559a6ba84eaa490ab631
Samuel Mata <Samuelab2@gmail.com>
2020-05-29 20:04:45 -0400

    practicing HOC and render props to retrieve data from the server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyles } from './styles/GlobalStyles'
-import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { ListOfPhotoCards } from './container/ListOfPhotoCards'
 import { Logo } from './components/Logo'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
-export const App = () => (
-  <div>
-    <GlobalStyles />
-    <Logo />
-    <ListOfCategories />
-    <ListOfPhotoCards />
-  </div>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+
+  return (
+    <div>
+      <GlobalStyles />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId}/>
+          : <>
+              <ListOfCategories />
+              <ListOfPhotoCards categoryId={1} />
+            </>
+      }
+    </div>
+)}
