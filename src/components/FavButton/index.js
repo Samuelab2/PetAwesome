81be95dc9ce47b77bc0c876550c0a4f59f4abee7
Samuel Mata <Samuelab2@gmail.com>
2020-05-30 20:07:40 -0400

    starting to build a pwa, install a webpack plugin and imported an image

--- src/components/FavButton/index.js
+++ src/components/FavButton/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FcLike, FcLikePlaceholder } from "react-icons/fc";
 import { Button } from './styles'
+import PropTypes from 'prop-types'
 
 export const FavButton = ({ liked, likes, onClick}) => {
   const Icon = liked ? FcLike : FcLikePlaceholder
@@ -9,3 +10,9 @@ export const FavButton = ({ liked, likes, onClick}) => {
       <Icon size='32px' /> {likes} likes!
     </Button>
 )}
+
+FavButton.propTypes = {
+  liked: PropTypes.bool.isRequired,
+  likes: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired
+}
